6bc224bbef86d1d5ce851375d3ce37cdb2ffc587
marcos <marcosviniciussouzadossantos19@gmail.com>
2024-11-19 09:55:27 -0300

    add swagger

--- EstagioREC/Program.cs
+++ EstagioREC/Program.cs
@@ -6,6 +6,7 @@ using EstagioREC.Repository.Implementations;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("Estagio"));
 
 builder.Services.AddScoped<IOrientadorRepository, OrientadorRepository>();
@@ -17,5 +18,12 @@ var app = builder.Build();
 
 DbInitializer.RetrieveFromSheets(app);
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.Run();
