698197927e9a2e93456f503091bc3f8f306a46f4
Marcos Vinícius Souza <marcosviniciussouzadossantos19@gmail.com>
2025-01-30 09:18:22 -0300

    Adicionando Cors

--- EstagioREC/Program.cs
+++ EstagioREC/Program.cs
@@ -1,9 +1,19 @@
 using EstagioREC.Application.Services;
 using EstagioREC.Persistence;
-using EstagioREC.Persistence.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+// Adiciona CORS permitindo todas as origens
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 
+// Configuração da persistência e aplicação
 builder.Services.ConfigurePersistenceApp(builder.Configuration);
 builder.Services.ConfigureApplicationApp();
 
@@ -14,6 +24,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 //DbInitializer.RetrieveFromSheets(app);
+
+// Aplica o middleware de CORS antes dos controllers
+app.UseCors("AllowAllOrigins");
+
 app.MapControllers();
 
 if (app.Environment.IsDevelopment())
